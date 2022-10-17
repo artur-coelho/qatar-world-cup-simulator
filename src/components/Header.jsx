513c84908479a6f8c9a587cce2da0d5de6231d9b
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-10-16 23:49:03 -0300

    added responsivity to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import html2canvas from 'html2canvas';
 import { AiOutlineDownload } from 'react-icons/ai';
 import FifaLogo from '../assets/logo/fifaLogo.svg?component';
+import { Col, Container, Row } from 'react-bootstrap';
 const Header = () => {
 	const takeScreenshot = async () => {
 		const canvas = await html2canvas(document.querySelector('#root'));
@@ -17,27 +18,21 @@ const Header = () => {
 		a.click();
 	};
 	return (
-		<section className='header'>
-			<article className='logo'>
-				<FifaLogo />
-			</article>
-			<article className='titleContainer'>
-				<div className='content'>
-					<h1>PRONÓSTICO</h1>
-					<h4>Mundial Qatar 2022</h4>
-					<p>
-						Arrastre el equipo de cada grupo a la posición en la que cree va a
-						clasificar.
-					</p>
-				</div>
-			</article>
-			<article className='buttonsSection'>
-				<button id='btnDownloader' onClick={takeScreenshot}>
-					<AiOutlineDownload />
-					<span>Download</span>
-				</button>
-			</article>
-			<article></article>
+		<section>
+			<Container>
+				<Row className='p-2 d-flex justify-content-center'>
+					<Col xs={0} sm={3} className='logo d-none d-md-block'>
+						<FifaLogo />
+					</Col>
+					<Col xs={12} md={6} className='titleContainer'>
+						<div className='content'>
+							<h1>Simulador</h1>
+							<h4>Copa do Mundo Catar 2022</h4>
+						</div>
+					</Col>
+					<Col xs={0} sm={3} className='d-none d-md-block'></Col>
+				</Row>
+			</Container>
 		</section>
 	);
 };
